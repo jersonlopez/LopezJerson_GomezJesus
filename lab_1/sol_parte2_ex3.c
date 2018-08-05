93fca158ad5c8edcbd1e054af7335e402562b947
jersonlopez <jersonlopez.1204@gmail.com>
2018-08-04 19:35:45 -0500

    el tres sin terminar

--- lab_1/sol_parte2_ex3.c
+++ lab_1/sol_parte2_ex3.c
@@ -1,54 +1,46 @@
 #include "stdio.h"
 #include "math.h"
 
-int suma(int x, int n);
+float suma(int x, int n);
 int factorial(int a);
 
 int main()
 {
     int x, n;
-    int result;
+    float result;
     printf("Introduzca el número x por favor: ");
     scanf("%d", &x);
     printf("Introduzca el número n por favor: ");
     scanf("%d", &n);
     result = suma(x, n);
-    for (int i=1; i <= n; i++){
-        int flag = 0;
-        if(i%2==0)
-            printf("0");
-        else{
-            flag = 1;
-            printf("1");
-        }
-        for(int j=2; j<= i; j++){            
-            if (flag == 0){
-                flag = 1;
-                printf("1");
-            }
-            else {
-                flag = 0;
-                printf("0");
-            }            
-        }
-        printf("\n");
-    }
-    return 0;
+
+    printf("La suma de la serie es: %.6f\n", result);
 }
 
-int suma(int x, int n)
+float suma(int x, int n)
 {
-    int result = 1;
-    for (int i = 1; i <= n; i++)
+    float result = 1;
+    int flag = 1;
+    for (int i = 2; i <= n; i += 2)
     {
-        result += pow(x, i);
+
+        if (flag == 1){
+            result -= pow(x, i)/factorial(i);
+            flag = 0;
+        }
+        else
+        {
+            result += pow(x, i)/factorial(i);
+            flag = 1;
+        }
     }
     return result;
 }
 
-int factorial (int a) {
-  int i,product=1;
-  for (i=2; i<=n; ++i)
-    product *= i;
-  return product;
+int factorial(int a)
+{
+    int i, product = 1;
+    for (i = 2; i <= a; ++i)
+        product *= i;
+    return product;
 }
