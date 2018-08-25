4a8a946b30b59909e563a62b0acde04e525e1ce4
jersonlopez <jersonlopez.1204@gmail.com>
2018-08-24 19:21:32 -0500

    i need to done

--- lab_2/sol_ex2.c
+++ lab_2/sol_ex2.c
@@ -9,26 +9,28 @@ int main()
 }
 
 void contarCaracter(char *array, char letter) {
-    int quantity = 0;
+    int quantity = 0, position = 0, i = 0;
     char *str = array;
 
     while(*str != '\0'){
         if(*str == letter){
+            if(position == 0) position = i;
             quantity += 1;
         }
         str++;
+        i++;
     }
 
     if(quantity == 0) 
     printf("-1\n");
     else
     printf("la cantidad de %c en la cadena es: %d\n", letter, quantity);
+    printf("y aparecio por primera vez en la posicion %d de la cadena \n", position);
 }
 
 void test(){
     char string[] = "codebabes";
     char letter = 'b';
-    
     printf("la cadena es: %s \n", string);
     contarCaracter(string, letter);
 }
