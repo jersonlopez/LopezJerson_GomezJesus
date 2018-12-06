6117fb8c5c240750845dce5a489a62209f1679c3
chucho <jesus.gomezo@udea.edu.co>
2018-12-06 16:45:12 -0500

    finalizado

--- shell/sources/execute_cd.c
+++ shell/sources/execute_cd.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 	{
 		printf("%sUsted se encuentra parado en: %s%s.\n",MARRON,BLANCO,cwd);
 	}
-	printf("valor %s\n",argv[1]);	
+	printf("valor %s\n",argv[1]);
     if (argv[1] == NULL)
 	{
 		printf("Error: %sEs necesario indicar el directorio.\n",ROJO);
@@ -22,8 +22,7 @@ int main(int argc, char *argv[])
 		if (chdir(argv[1]) != 0)
 		{
 			printf("Error: %sEl directorio ingresado no existe o lo escribio de forma incorrecta\n",ROJO);
-		}
-		
+		}		
 		cwd = getcwd(buff, PATH_MAX + 1);
 		if (cwd != NULL)
 		{
